74a1edbe4ff5ab3118af46ade76bfd916f8f910b
Ufuk Bombar <ufukbombar@bordatech.com>
2021-04-03 14:28:02 +0300

    Fix yet another bug

--- src/Main.ts
+++ src/Main.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.send('Working correctly!');
