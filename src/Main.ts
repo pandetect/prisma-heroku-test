ba170f9a041ce70bb86a9cd9d76d39c6c6545312
Ufuk Bombar <ufukbombar@bordatech.com>
2021-04-03 14:06:16 +0300

    Add test express code

--- src/Main.ts
+++ src/Main.ts
@@ -1,2 +1,12 @@
+import express from 'express';
 
-console.log('success');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Working correctly!');
+});
+
+app.listen(port, () => {
+    console.log(`Started on http://localhost:${port}`);
+});
